ac188013bd7639123a94b25517a0c119a224ba1c
puskuriuday <udayijbc@gmail.com>
2025-09-18 17:35:08 +0000

    adding p2p folder without any code

---
+++ p2p/transport.go
@@ -0,0 +1,14 @@
+package p2p
+
+
+// peer is an interface that represents a remote node in the network
+type Peer interface {
+
+}
+
+// transport is anything that can handles communication between peers
+// the nodes in the network can have multiple transports
+
+type Transport interface {
+
+}
